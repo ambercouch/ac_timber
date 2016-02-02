ded8760c161e043a43e491d440c5a7134ecd0fff
Richiearnold <richard@ambercouch.co.uk>
2016-02-02 08:06:49 +0000

    typo

--- functions.php
+++ functions.php
@@ -1,5 +1,5 @@
 <?php
-//Love, love, love, love all you functions
+//Love, love, love, love all you functions.
 
 require_once get_template_directory() . '/lib/functions--timber.php';
 require_once get_template_directory() . '/lib/functions--theme-setup.php';
