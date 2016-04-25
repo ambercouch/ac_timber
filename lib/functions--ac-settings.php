61f10ffccae3afb7f716dc33c13a9c88d84366e1
richiearnold <richard@ambercouch.co.uk>
2016-04-25 11:40:53 +0100

    added default image setting

--- lib/functions--ac-settings.php
+++ lib/functions--ac-settings.php
@@ -4,6 +4,7 @@
     function acSettings()
     {
         $settings['svgLogo'] = null;
+        $settings['defaultImage'] = null;
         $settings['typeKitId'] = 'hej7ovh';
         return $settings;
     }
