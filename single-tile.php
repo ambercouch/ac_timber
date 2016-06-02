dad53b6abb7d37da469a4952cb3ca988ec662bbd
richiearnold <richard@ambercouch.co.uk>
2016-06-02 10:28:35 +0100

    Updated tile styles

--- single-tile.php
+++ single-tile.php
@@ -10,11 +10,26 @@ if ( ! class_exists( 'Timber' ) ) {
     return;
 }
 
+$tile_colours = get_the_terms($post->ID, 'tile_colour');
+foreach ($tile_colours as $key => $colour){
+    $tile_colours[$key] = $colour->name;
+}
+
+$tile_material = get_the_terms($post->ID, 'tile_material');
+foreach ($tile_material as $key => $material){
+    $tile_material[$key] = $material->name;
+}
+
 $context = Timber::get_context();
 $context['posts'] = Timber::get_posts();
 $context['footer_widgets'] = Timber::get_widgets('Footer');
 $context['primary_widgets'] = Timber::get_widgets('Primary');
 $context['template_class'] = '--tile';
+$context['tile_colour'] = $tile_colours;
+$context['tile_material'] = $tile_material;
+//echo '<pre>';
+//print_r($context['tile_colour']);
+//die;
 $templates = array( 'tile.twig' , 'single.twig');
 
 Timber::render( $templates, $context );
