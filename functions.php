e27ac511c75cecaa6d41b780372b97cde8a82e14
richiearnold <richard@ambercouch.co.uk>
2016-06-13 08:25:21 +0100

    added general options page

--- functions.php
+++ functions.php
@@ -77,6 +77,12 @@ if( function_exists('acf_add_options_page') ) {
     ));
 
     acf_add_options_sub_page(array(
+        'page_title' 	=> 'General Settings',
+        'menu_title'	=> 'General Settings',
+        'parent_slug'	=> 'theme-general-settings',
+    ));
+
+    acf_add_options_sub_page(array(
         'page_title' 	=> 'Banner Settings',
         'menu_title'	=> 'Banner Settings',
         'parent_slug'	=> 'theme-general-settings',
