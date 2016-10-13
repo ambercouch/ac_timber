fe904b2caad58b4986a6cb2aea3a0c59507bf6f2
richiearnold <richard@ambercouch.co.uk>
2016-10-13 08:46:35 +0100

    added cpt and widget template

---
+++ lib/functions--widgets.php
@@ -0,0 +1,170 @@
+<?php
+
+class Ac_Custom_Loop extends WP_Widget {
+
+  public function __construct() {
+
+    parent::__construct(
+            'ac_custom_loop', // Base ID
+            'Custom Loop', // Name
+            array('description' => __('Custom loops', 'ambercouch'),) // Args
+    );
+  }
+
+  public function form($instance) {
+    if (isset($instance['title'])) {
+      $title = $instance['title'];
+    } else {
+      $title = __('New title', 'ambercouch');
+    }
+    if (isset($instance['post_type'])) {
+      $post_type = $instance['post_type'];
+    } else {
+      $post_type = __('', 'ambercouch');
+    }
+    if (isset($instance['show'])) {
+      $show = $instance['show'];
+    } else {
+      $show = __('1', 'ambercouch');
+    }
+    ?>
+    <p>
+      <label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label>
+      <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($title); ?>" />
+    </p>
+    <p>
+      <label for="<?php echo $this->get_field_id('post_type'); ?>"><?php _e('Post Type:'); ?></label>
+      <input class="widefat" id="<?php echo $this->get_field_id('post_type'); ?>" name="<?php echo $this->get_field_name('post_type'); ?>" type="text" value="<?php echo esc_attr($post_type); ?>" />
+    </p>
+    <p>
+      <label for="<?php echo $this->get_field_id('show'); ?>"><?php _e('Number of posts to show:') ?></label>
+      <input id="<?php echo $this->get_field_id('show'); ?>" type="text" size="3" value="<?php echo esc_attr($show); ?>" name="<?php echo $this->get_field_name('show'); ?>">
+    </p>
+    <?php
+  }
+
+  public function update($new_instance, $old_instance) {
+    $instance = array();
+    $instance['title'] = strip_tags($new_instance['title']);
+    $instance['post_type'] = strip_tags($new_instance['post_type']);
+    $instance['show'] = strip_tags($new_instance['show']);
+
+    return $instance;
+  }
+
+  public function widget($args, $instance) {
+
+    extract($args);
+    global $wp_query;
+    $temp_q = $wp_query;
+    $wp_query = null;
+    $wp_query = new WP_Query();
+    $wp_query->query(array($tax => $cat,
+        'post_type' => $instance['post_type'],
+        'showposts' => $instance['show'],
+        'orderby' => 'menu_order',
+        'order' => 'ASC')
+    );
+    echo $before_widget;
+    echo $before_title;
+    echo  $instance['title'];
+    echo $after_title;
+    if (have_posts()) :
+      ?>
+      <ul class="<?php echo $instance['post_type'] ?>-list">
+      <?php while (have_posts()): the_post(); ?>
+
+          <?php get_template_part('content', $instance['post_type']) ?>
+
+        <?php endwhile; ?>
+      </ul>
+      <?php
+      endif;
+      echo $after_widget;
+      $wp_query = $temp_q;
+    }
+
+  }
+
+  register_widget('Ac_Custom_Loop');
+
+class Act_Tax_Link extends WP_Widget {
+
+  public function __construct() {
+
+    parent::__construct(
+        'act_taxonomy_info_link', // Base ID
+        'Taxonomy Term Link', // Name
+        array('description' => __('Displays the content in a taxonomy term.', 'act'),) // Args
+    );
+  }
+
+  public function form($instance) {
+    if (isset($instance['title'])) {
+      $title = $instance['title'];
+    } else {
+      $title = __('New title', 'ambercouch');
+    }
+    if (isset($instance['post_type'])) {
+      $post_type = $instance['post_type'];
+    } else {
+      $post_type = __('', 'ambercouch');
+    }
+    if (isset($instance['tax_term'])) {
+      $tax_term = $instance['tax_term'];
+    } else {
+      $tax_term = __('', 'act');
+    }
+    if (isset($instance['show'])) {
+      $show = $instance['show'];
+    } else {
+      $show = __('1', 'ambercouch');
+    }
+
+    $terms = get_terms( array(
+        'taxonomy' => 'service_category',
+        'hide_empty' => false,
+    ) );
+
+       ?>
+
+      <p>
+      <label for="<?php echo $this->get_field_id('tax_term'); ?>"><?php _e('Taxonomy Term:'); ?></label>
+      <select  class="widefat" id="<?php echo $this->get_field_id('tax_term'); ?>" name="<?php echo $this->get_field_name('tax_term'); ?>">
+        <?php foreach ($terms as $term) : ?>
+          <option <?php echo ($tax_term == $term->term_id )? 'selected' : 'test' ?> value="<?php echo $term->term_id; ?>"><?php echo $term->name; ?></option>
+        <?php endforeach; ?>
+      </select>
+      </p>
+    <?php
+  }
+
+  public function update($new_instance, $old_instance) {
+    $instance = array();
+    $instance['tax_term'] = strip_tags($new_instance['tax_term']);
+    return $instance;
+  }
+
+  public function widget($args, $instance) {
+
+    extract($args);
+
+$term = get_term($instance['tax_term'], 'service_category');
+    $icon = get_field('category_icon', 'service_category_4');
+
+    echo $before_widget;
+
+    echo '<img src="' . $icon['url'] . '" >';
+    echo $before_title;
+    echo  $term->name;
+    echo $after_title;
+    echo  $term->;
+
+
+    echo $after_widget;
+
+  }
+
+}
+
+register_widget('Act_Tax_Link');
