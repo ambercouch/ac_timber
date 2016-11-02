55c8f2f524ee0dbf0d7b63f85eb4ef794f036773
richiearnold <richard@ambercouch.co.uk>
2016-11-02 16:25:18 +0000

    update shortcode

--- lib/functions--ac-shortcode.php
+++ lib/functions--ac-shortcode.php
@@ -19,12 +19,16 @@ add_shortcode('act_bloginfo', 'act_bloginfo');
 
 function act_option_logo( $atts ) {
 
+    extract(shortcode_atts(array(
+        'class' => 'company-logo',
+    ), $atts));
+
     $logo = get_field('company_logo', 'option');
     if ($logo == ''){
         return;
     }
 
-    $output = '<img src="'.$logo['url'].'" alt="'.$logo['alt'].'" />';
+    $output = '<img class="'.$class.'" src="'.$logo['url'].'" alt="'.$logo['alt'].'" />';
     return $output  ;
 }
 add_shortcode('act_option_logo', 'act_option_logo');
