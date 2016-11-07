53bf7bd224728650a451134dd3b7960d278f4908
richiearnold <richard@ambercouch.co.uk>
2016-11-07 09:51:50 +0000

    update wideget areas local

--- lib/functions--widget-areas.php
+++ lib/functions--widget-areas.php
@@ -2,12 +2,14 @@
 /**
  * Register widget area.
  *
+ * To add a new sidebar edit functions--ac-settings
+ *
  * @link http://codex.wordpress.org/Function_Reference/register_sidebar
  */
 function _act_widgets_init() {
     foreach( unserialize(SIDEBARS) as $sidebar ) {
         register_sidebar(array(
-            'name' => __($sidebar , '_s'),
+            'name' => __($sidebar , '_act'),
             'id' => 'aside-'.strtolower($sidebar),
             'description' => '',
             'before_widget' => '<aside id="%1$s" class="'.strtolower($sidebar).'__widget %2$s"><div  class="widget widget--'.strtolower($sidebar).'">',
