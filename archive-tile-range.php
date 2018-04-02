6d0920fada365cbf134b8232fa060ffee4df2dde
Richiearnold <richard@ambercouch.co.uk>
2018-04-02 17:37:25 +0100

    Add tile range basic template

---
+++ archive-tile-range.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * The main template file
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists
+ *
+ * Methods for TimberHelper can be found in the /lib sub-directory
+ *
+ * @package  WordPress
+ * @subpackage  Timber
+ * @since   Timber 0.1
+ */
+if ( ! class_exists( 'Timber' ) ) {
+    echo 'Timber not activated. Make sure you activate the plugin in <a href="/wp-admin/plugins.php#timber">/wp-admin/plugins.php</a>';
+    return;
+}
+
+global $post;
+
+$args = array(
+    'taxonomy' => 'tile_material',
+    'hide_empty' => false,
+);
+$tags_array = get_terms($args);
+$fillterItems['tile_material']['name'] = "Tile Material";
+$fillterItems['tile_material']['tags'] = $tags_array;
+
+$args = array(
+    'taxonomy' => 'tile_colour',
+    'hide_empty' => false,
+);
+$tags_array = get_terms($args);
+$fillterItems['tile_colour']['name'] = "Tile Colour";
+$fillterItems['tile_colour']['tags'] = $tags_array;
+
+$args = array(
+    'taxonomy' => 'tile_size',
+    'hide_empty' => false,
+);
+$tags_array = get_terms($args);
+$fillterItems['tile_size']['name'] = "Tile Size";
+$fillterItems['tile_size']['tags'] = $tags_array;
+
+$context = Timber::get_context();
+$context['posts'] = Timber::get_posts();
+$context['footer_widgets'] = Timber::get_widgets('Footer');
+$context['primary_widgets'] = Timber::get_widgets('Primary');
+$context['template_class'] = '--archive-tile';
+$context['tags'] = '--archive-tile';
+$context['tile_filters'] = $fillterItems;
+$context['archive_empty'] = ($post)? 'has-posts' : 'is-empty';
+$templates = array( 'archive-tile-range.twig' , 'index.twig');
+
+
+Timber::render( $templates, $context );
--- lib/functions--cpt.php
+++ lib/functions--cpt.php
@@ -1,4 +1,37 @@
 <?php
+function  tvb_tile_ranges() {
+//Tiles
+    $labels = array(
+        'name' => _x('Tile Ranges', 'post type general name'),
+        'singular_name' => _x('Tile Range', 'post type singular name'),
+        'add_new' => _x('Add New', 'Range'),
+        'add_new_item' => __('Add New Range'),
+        'edit_item' => __('Edit Tile Range'),
+        'new_item' => __('New Range'),
+        'all_items' => __('All Ranges'),
+        'view_item' => __('View Tile Range'),
+        'search_items' => __('Search Tile Ranges'),
+        'not_found' => __('No Tile Ranges found'),
+        'not_found_in_trash' => __('No Tile Ranges found in the trash'),
+        'parent_item_colon' => '',
+        'menu_name' => 'Tile Ranges'
+    );
+    $args = array(
+        'labels' => $labels,
+        'menu_icon' => 'dashicons-layout',
+        'description' => 'Tile Ranges for the tile gallery',
+        'public' => true,
+        'menu_position' => 20,
+        'supports' => array('title'),
+        'has_archive' => 'tile-ranges'
+    );
+    register_post_type('tile-range', $args);
+
+
+}
+
+add_action('init', 'tvb_tile_ranges');
+
 function  tvb_tiles() {
 //Tiles
     $labels = array(
@@ -18,7 +51,7 @@ function  tvb_tiles() {
     );
     $args = array(
         'labels' => $labels,
-        'menu_icon' => 'dashicons-layout',
+        'menu_icon' => 'dashicons-admin-page',
         'description' => 'Tiles for the tile gallery',
         'public' => true,
         'menu_position' => 20,
@@ -27,17 +60,18 @@ function  tvb_tiles() {
     );
     register_post_type('tile', $args);
 
-//Tile colours
+//Tile Groups
+    //Remove because it does not work as a Taxonomy
     $labels = array(
-        'name'              => _x( 'Tile Colours', 'taxonomy general name' ),
-        'singular_name'     => _x( 'Tile Colour', 'taxonomy singular name' ),
-        'search_items'      => __( 'Search Tile Colours' ),
-        'all_items'         => __( 'All Tile Colours' ),
-        'edit_item'         => __( 'Edit Tile Colour' ),
-        'update_item'       => __( 'Update Tile Colour' ),
-        'add_new_item'      => __( 'Add New Tile Colour' ),
-        'new_item_name'     => __( 'New Tile Colour Name' ),
-        'menu_name'         => __( 'Tile Colour' ),
+        'name'              => _x( 'Tile Groups', 'taxonomy general name' ),
+        'singular_name'     => _x( 'Tile Group', 'taxonomy singular name' ),
+        'search_items'      => __( 'Search Tile Groups' ),
+        'all_items'         => __( 'All Tile Groups' ),
+        'edit_item'         => __( 'Edit Tile Group' ),
+        'update_item'       => __( 'Update Tile Group' ),
+        'add_new_item'      => __( 'Add New Tile Group' ),
+        'new_item_name'     => __( 'New Tile Group Name' ),
+        'menu_name'         => __( 'Tile Group' ),
     );
 
     $args = array(
@@ -46,60 +80,16 @@ function  tvb_tiles() {
         'show_ui'           => true,
         'show_admin_column' => true,
         'query_var'         => true,
-        'rewrite'           => array( 'slug' => 'colour' ),
+        'rewrite'           => array( 'slug' => 'group' ),
     );
 
-    register_taxonomy( 'tile_colour', array( 'tile' ), $args );
-//Tile Material
-    $labels = array(
-        'name'              => _x( 'Tile Material', 'taxonomy general name' ),
-        'singular_name'     => _x( 'Tile Material', 'taxonomy singular name' ),
-        'search_items'      => __( 'Search Tile Material' ),
-        'all_items'         => __( 'All Tile Material' ),
-        'edit_item'         => __( 'Edit Tile Material' ),
-        'update_item'       => __( 'Update Tile Material' ),
-        'add_new_item'      => __( 'Add New Tile Material' ),
-        'new_item_name'     => __( 'New Tile Material Name' ),
-        'menu_name'         => __( 'Tile Material' ),
-    );
+    //register_taxonomy( 'tile_group', array( 'tile' ), $args );
 
-    $args = array(
-        'hierarchical'      => false,
-        'labels'            => $labels,
-        'show_ui'           => true,
-        'show_admin_column' => true,
-        'query_var'         => true,
-        'rewrite'           => array( 'slug' => 'colour' ),
-    );
-
-    register_taxonomy( 'tile_material', array( 'tile' ), $args );
-//Tile size
-    $labels = array(
-        'name'              => _x( 'Tile Size', 'taxonomy general name' ),
-        'singular_name'     => _x( 'Tile Size', 'taxonomy singular name' ),
-        'search_items'      => __( 'Search Tile Size' ),
-        'all_items'         => __( 'All Tile Sizes' ),
-        'edit_item'         => __( 'Edit Tile Size' ),
-        'update_item'       => __( 'Update Tile Size' ),
-        'add_new_item'      => __( 'Add New Tile Size' ),
-        'new_item_name'     => __( 'New Tile Size' ),
-        'menu_name'         => __( 'Tile Size' ),
-    );
-
-    $args = array(
-        'hierarchical'      => false,
-        'labels'            => $labels,
-        'show_ui'           => true,
-        'show_admin_column' => true,
-        'query_var'         => true,
-        'rewrite'           => array( 'slug' => 'size' ),
-    );
-
-    register_taxonomy( 'tile_size', array( 'tile' ), $args );
 
 }
 
 add_action('init', 'tvb_tiles');
+
 
 
 function  tvb_work() {
