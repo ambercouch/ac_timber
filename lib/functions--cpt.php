429464e96218ac5e008d1735d325faa1eba19f04
Richie Arnold <richard@ambercouch.co.uk>
2018-05-11 20:17:07 +0100

    Add Slide cpt

--- lib/functions--cpt.php
+++ lib/functions--cpt.php
@@ -1,6 +1,57 @@
 <?php
 function  act_cpt() {
-//Tiles
+////ACTcpts
+//    $labels = array(
+//        'name' => _x('ACTcpts', 'post type general name'),
+//        'singular_name' => _x('ACTcpt', 'post type singular name'),
+//        'add_new' => _x('Add New', 'ACTcpt'),
+//        'add_new_item' => __('Add New ACTcpt'),
+//        'edit_item' => __('Edit ACTcpt'),
+//        'new_item' => __('New ACTcpt'),
+//        'all_items' => __('All ACTcpts'),
+//        'view_item' => __('View ACTcpt'),
+//        'search_items' => __('Search ACTcpts'),
+//        'not_found' => __('No ACTcpts found'),
+//        'not_found_in_trash' => __('No ACTcpts found in the trash'),
+//        'parent_item_colon' => '',
+//        'menu_name' => 'ACTcpts'
+//    );
+//    $args = array(
+//        'labels' => $labels,
+//        'menu_icon' => 'dashicons-store',
+//        'description' => 'ACTcpts offered',
+//        'public' => true,
+//        'menu_position' => 20,
+//        'supports' => array('title','editor','author','thumbnail','excerpt','trackbacks','custom-fields','comments','revisions','page-attributes','post-formats'),
+//        'has_archive' => 'actcpt'
+//    );
+//    register_post_type('actcpt', $args);
+//
+////ACTcpt Categories
+//    $labels = array(
+//        'name'              => _x( 'ACTcpt Categories', 'taxonomy general name' ),
+//        'singular_name'     => _x( 'ACTcpt Category', 'taxonomy singular name' ),
+//        'search_items'      => __( 'Search ACTcpt Categories' ),
+//        'all_items'         => __( 'All ACTcpt Categories' ),
+//        'edit_item'         => __( 'Edit ACTcpt Category' ),
+//        'update_item'       => __( 'Update ACTcpt Category' ),
+//        'add_new_item'      => __( 'Add New ACTcpt Category' ),
+//        'new_item_name'     => __( 'New Tile ACTcpt Category' ),
+//        'menu_name'         => __( 'ACTcpt Categories' ),
+//    );
+//
+//    $args = array(
+//        'hierarchical'      => false,
+//        'labels'            => $labels,
+//        'show_ui'           => true,
+//        'show_admin_column' => true,
+//        'query_var'         => true,
+//        'rewrite'           => array( 'slug' => 'actcpt-category' ),
+//    );
+//
+//    register_taxonomy( 'actcpt_category', array( 'actcpt' ), $args );
+    
+    //Services
     $labels = array(
         'name' => _x('Services', 'post type general name'),
         'singular_name' => _x('Service', 'post type singular name'),
@@ -50,6 +101,57 @@ function  act_cpt() {
     );
 
     register_taxonomy( 'service_category', array( 'service' ), $args );
+
+    //Slides
+    $labels = array(
+        'name' => _x('Slides', 'post type general name'),
+        'singular_name' => _x('Slide', 'post type singular name'),
+        'add_new' => _x('Add New', 'Slide'),
+        'add_new_item' => __('Add New Slide'),
+        'edit_item' => __('Edit Slide'),
+        'new_item' => __('New Slide'),
+        'all_items' => __('All Slides'),
+        'view_item' => __('View Slide'),
+        'search_items' => __('Search Slides'),
+        'not_found' => __('No Slides found'),
+        'not_found_in_trash' => __('No Slides found in the trash'),
+        'parent_item_colon' => '',
+        'menu_name' => 'Slides'
+    );
+    $args = array(
+        'labels' => $labels,
+        'menu_icon' => 'dashicons-chart-area',
+        'description' => 'Slides offered',
+        'public' => true,
+        'menu_position' => 20,
+        'supports' => array('title','editor','author','thumbnail','excerpt','trackbacks','custom-fields','comments','revisions','page-attributes','post-formats'),
+        'has_archive' => 'slide'
+    );
+    register_post_type('slide', $args);
+
+//Slide Categories
+    $labels = array(
+        'name'              => _x( 'Slide Categories', 'taxonomy general name' ),
+        'singular_name'     => _x( 'Slide Category', 'taxonomy singular name' ),
+        'search_items'      => __( 'Search Slide Categories' ),
+        'all_items'         => __( 'All Slide Categories' ),
+        'edit_item'         => __( 'Edit Slide Category' ),
+        'update_item'       => __( 'Update Slide Category' ),
+        'add_new_item'      => __( 'Add New Slide Category' ),
+        'new_item_name'     => __( 'New Tile Slide Category' ),
+        'menu_name'         => __( 'Slide Categories' ),
+    );
+
+    $args = array(
+        'hierarchical'      => false,
+        'labels'            => $labels,
+        'show_ui'           => true,
+        'show_admin_column' => true,
+        'query_var'         => true,
+        'rewrite'           => array( 'slug' => 'slide-category' ),
+    );
+
+    register_taxonomy( 'slide_category', array( 'slide' ), $args );
 
 }
 
