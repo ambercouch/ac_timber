6601aa4b55bc4792adc6dfc062164375fe99707c
Richiearnold <richard@ambercouch.co.uk>
2018-06-11 13:11:55 +0100

    Update tile gallery to set it lives

--- archive-tile-range.php
+++ archive-tile-range.php
@@ -50,7 +50,7 @@ $context['primary_widgets'] = Timber::get_widgets('Primary');
 $context['template_class'] = '--archive-tile';
 $context['tags'] = '--archive-tile';
 $context['tile_filters'] = $fillterItems;
-$context['archive_empty'] = (is_user_logged_in())? 'has-posts' : 'is-empty';
+$context['archive_empty'] = ($post)? 'has-posts' : 'is-empty';
 $context['tile_list_image'] = (is_array(get_field('list_image', $post->id )))?
     get_field('list_image', $post->id )['sizes']['feature500x600'] : get_field('in_situ_image', $post->id )['sizes']['feature500x600'];
 $templates = array( 'archive-tile-range.twig' , 'index.twig');
