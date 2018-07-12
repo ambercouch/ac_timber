6557551b0e5443993b4e43a1a956f42dcc8350f4
Richiearnold <richard@ambercouch.co.uk>
2018-07-12 07:13:55 +0100

    Add typekit ID

--- lib/functions--ac-settings.php
+++ lib/functions--ac-settings.php
@@ -5,7 +5,7 @@
     {
         $settings['svgLogo'] = null;
         $settings['defaultImage'] = null;
-        $settings['typeKitId'] = 'hej7ovh';
+        $settings['typeKitId'] = 'ylr5rpl';
         $settings['sidebars'] = unserialize(ACT_SIDEBARS);
         $settings['bannerImg']['url'] = get_stylesheet_directory_uri().'/assets/images/jpg/act-default.jpg';
         $settings['bannerImg']['alt'] = 'AmberCouch Timber Theme';
