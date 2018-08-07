a9f3f8b411e7f310c3104749900d15f740185c87
Richie Arnold <richard@ambercouch.co.uk>
2018-08-07 14:51:11 +0100

    Add project custom post type

--- lib/functions--cpt.php
+++ lib/functions--cpt.php
@@ -1,6 +1,6 @@
 <?php
 function  act_cpt() {
-//Tiles
+    //Services
     $labels = array(
         'name' => _x('Services', 'post type general name'),
         'singular_name' => _x('Service', 'post type singular name'),
@@ -50,6 +50,57 @@ function  act_cpt() {
     );
 
     register_taxonomy( 'service_category', array( 'service' ), $args );
+
+    //Projects
+    $labels = array(
+        'name' => _x('Projects', 'project type general name'),
+        'singular_name' => _x('Project', 'project type singular name'),
+        'add_new' => _x('Add New', 'Project'),
+        'add_new_item' => __('Add New Project'),
+        'edit_item' => __('Edit Project'),
+        'new_item' => __('New Project'),
+        'all_items' => __('All Projects'),
+        'view_item' => __('View Project'),
+        'search_items' => __('Search Projects'),
+        'not_found' => __('No Projects found'),
+        'not_found_in_trash' => __('No Projects found in the trash'),
+        'parent_item_colon' => '',
+        'menu_name' => 'Projects'
+    );
+    $args = array(
+        'labels' => $labels,
+        'menu_icon' => 'dashicons-store',
+        'description' => 'Projects offered',
+        'public' => true,
+        'menu_position' => 20,
+        'supports' => array('title','editor','author','thumbnail','excerpt','trackbacks','custom-fields','comments','revisions','page-attributes','project-formats'),
+        'has_archive' => 'project'
+    );
+    register_project_type('project', $args);
+
+//Project Categories
+    $labels = array(
+        'name'              => _x( 'Project Categories', 'taxonomy general name' ),
+        'singular_name'     => _x( 'Project Category', 'taxonomy singular name' ),
+        'search_items'      => __( 'Search Project Categories' ),
+        'all_items'         => __( 'All Project Categories' ),
+        'edit_item'         => __( 'Edit Project Category' ),
+        'update_item'       => __( 'Update Project Category' ),
+        'add_new_item'      => __( 'Add New Project Category' ),
+        'new_item_name'     => __( 'New Tile Project Category' ),
+        'menu_name'         => __( 'Project Categories' ),
+    );
+
+    $args = array(
+        'hierarchical'      => false,
+        'labels'            => $labels,
+        'show_ui'           => true,
+        'show_admin_column' => true,
+        'query_var'         => true,
+        'rewrite'           => array( 'slug' => 'project-category' ),
+    );
+
+    register_taxonomy( 'project_category', array( 'project' ), $args );
 
 }
 
