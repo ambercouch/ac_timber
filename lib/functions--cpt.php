75df3dbe1fda78868764a6a1ebaab0a8fca92f46
Richie Arnold <richard@ambercouch.co.uk>
2018-08-15 12:18:41 +0100

    add brand cpt

--- lib/functions--cpt.php
+++ lib/functions--cpt.php
@@ -50,6 +50,58 @@ function  act_cpt() {
     );
 
     register_taxonomy( 'service_category', array( 'service' ), $args );
+    
+    //BRANDS
+
+    $labels = array(
+        'name' => _x('Brands', 'post type general name'),
+        'singular_name' => _x('Brand', 'post type singular name'),
+        'add_new' => _x('Add New', 'Brand'),
+        'add_new_item' => __('Add New Brand'),
+        'edit_item' => __('Edit Brand'),
+        'new_item' => __('New Brand'),
+        'all_items' => __('All Brands'),
+        'view_item' => __('View Brand'),
+        'search_items' => __('Search Brands'),
+        'not_found' => __('No Brands found'),
+        'not_found_in_trash' => __('No Brands found in the trash'),
+        'parent_item_colon' => '',
+        'menu_name' => 'Brands'
+    );
+    $args = array(
+        'labels' => $labels,
+        'menu_icon' => 'dashicons-products',
+        'description' => 'Brands offered',
+        'public' => true,
+        'menu_position' => 20,
+        'supports' => array('title','editor','author','thumbnail','excerpt','trackbacks','custom-fields','comments','revisions','page-attributes','post-formats'),
+        'has_archive' => 'brand'
+    );
+    register_post_type('brand', $args);
+
+//Brand Categories
+    $labels = array(
+        'name'              => _x( 'Brand Categories', 'taxonomy general name' ),
+        'singular_name'     => _x( 'Brand Category', 'taxonomy singular name' ),
+        'search_items'      => __( 'Search Brand Categories' ),
+        'all_items'         => __( 'All Brand Categories' ),
+        'edit_item'         => __( 'Edit Brand Category' ),
+        'update_item'       => __( 'Update Brand Category' ),
+        'add_new_item'      => __( 'Add New Brand Category' ),
+        'new_item_name'     => __( 'New Tile Brand Category' ),
+        'menu_name'         => __( 'Brand Categories' ),
+    );
+
+    $args = array(
+        'hierarchical'      => false,
+        'labels'            => $labels,
+        'show_ui'           => true,
+        'show_admin_column' => true,
+        'query_var'         => true,
+        'rewrite'           => array( 'slug' => 'brand-category' ),
+    );
+
+    register_taxonomy( 'brand_category', array( 'brand' ), $args );
 
 }
 
