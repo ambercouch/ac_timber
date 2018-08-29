f74cefc42556576aebddc437ffb4829dce93cf31
Richie Arnold <richard@ambercouch.co.uk>
2018-08-29 10:10:36 +0100

    Add act_contact short code

--- lib/functions--ac-shortcode.php
+++ lib/functions--ac-shortcode.php
@@ -25,3 +25,41 @@ function act_theme_settings( $atts ) {
 }
 
 add_shortcode('act_theme_settings', 'act_theme_settings');
+
+function act_contact( $atts ) {
+    extract(shortcode_atts(array(
+        'row' => '',
+    ), $atts));
+    //return 'contact' . $row;
+    $contact_items = get_field('contact', 'options');
+    $contact_item = $contact_items[$row - 1];
+    $contact_type = $contact_item['contact_type'];
+    $contact_label = $contact_item['contact_label'];
+    $contact_value_key = 'contact_'.$contact_type;
+    $contact_value = $contact_item[$contact_value_key];
+
+    switch ($contact_type){
+        case 'email':
+            $contact_link = '<a href="mailto:'.$contact_value.'">'.$contact_value.'</a>';
+            break;
+        case 'telephone':
+            $contact_link = '<a href="tel:'.$contact_value.'">'.$contact_value.'</a>';
+            break;
+        case 'website':
+            $contact_link = '<a href="'.$contact_value.'">'.$contact_value.'</a>';
+            break;
+        case 'other':
+            $contact_link = $contact_value;
+            break;
+        default :
+            $contact_link = $contact_value;
+    }
+    $output = '';
+    $output .= $contact_label;
+    $output .= ' : ';
+    $output .= $contact_link;
+
+    return $output;
+}
+
+add_shortcode('act_contact', 'act_contact');
