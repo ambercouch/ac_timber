aaf112f9a8f42ff16fbf966cb7de27dc6c4143d5
Richie Arnold <richard@ambercouch.co.uk>
2018-09-17 11:42:09 +0100

    Add acf service category custom fields

--- functions.php
+++ functions.php
@@ -10,6 +10,8 @@ require_once get_template_directory() . '/lib/functions--cpt.php';
 require_once get_template_directory() . '/lib/functions--widgets.php';
 require_once get_template_directory() . '/lib/acf/functions--acf-options.php';
 require_once get_template_directory() . '/lib/acf/functions--acf-page-settings.php';
+require_once get_template_directory() . '/lib/acf/functions--acf-service-category.php';
+
 require_once get_template_directory() . '/lib/functions--template-tags.php';
 require_once get_template_directory() . '/lib/admin/functions--admin-clean.php';
 require_once get_template_directory() . '/lib/admin/functions--admin-widgets.php';
