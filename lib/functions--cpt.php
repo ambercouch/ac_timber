4e89fb1f116f1e7beb9d0b26b4be7270a297371d
Richie Arnold <richard@ambercouch.co.uk>
2018-10-11 08:41:11 +0100

    update CPT and masthead styles

--- lib/functions--cpt.php
+++ lib/functions--cpt.php
@@ -2,42 +2,42 @@
 function  act_cpt() {
 //Tiles
     $labels = array(
-        'name' => _x('Services', 'post type general name'),
-        'singular_name' => _x('Service', 'post type singular name'),
-        'add_new' => _x('Add New', 'Service'),
-        'add_new_item' => __('Add New Service'),
-        'edit_item' => __('Edit Service'),
-        'new_item' => __('New Service'),
-        'all_items' => __('All Services'),
-        'view_item' => __('View Service'),
-        'search_items' => __('Search Services'),
-        'not_found' => __('No Services found'),
-        'not_found_in_trash' => __('No Services found in the trash'),
+        'name' => _x('Treatments', 'post type general name'),
+        'singular_name' => _x('Treatment', 'post type singular name'),
+        'add_new' => _x('Add New', 'Treatment'),
+        'add_new_item' => __('Add New Treatment'),
+        'edit_item' => __('Edit Treatment'),
+        'new_item' => __('New Treatment'),
+        'all_items' => __('All Treatments'),
+        'view_item' => __('View Treatment'),
+        'search_items' => __('Search Treatments'),
+        'not_found' => __('No Treatments found'),
+        'not_found_in_trash' => __('No Treatments found in the trash'),
         'parent_item_colon' => '',
-        'menu_name' => 'Services'
+        'menu_name' => 'Treatments'
     );
     $args = array(
         'labels' => $labels,
         'menu_icon' => 'dashicons-store',
-        'description' => 'Services offered',
+        'description' => 'Treatments offered',
         'public' => true,
         'menu_position' => 20,
         'supports' => array('title','editor','author','thumbnail','excerpt','trackbacks','custom-fields','comments','revisions','page-attributes','post-formats'),
-        'has_archive' => 'service'
+        'has_archive' => 'treatment'
     );
-    register_post_type('service', $args);
+    register_post_type('treatment', $args);
 
-//Service Categories
+//Treatment Categories
     $labels = array(
-        'name'              => _x( 'Service Categories', 'taxonomy general name' ),
-        'singular_name'     => _x( 'Service Category', 'taxonomy singular name' ),
-        'search_items'      => __( 'Search Service Categories' ),
-        'all_items'         => __( 'All Service Categories' ),
-        'edit_item'         => __( 'Edit Service Category' ),
-        'update_item'       => __( 'Update Service Category' ),
-        'add_new_item'      => __( 'Add New Service Category' ),
-        'new_item_name'     => __( 'New Tile Service Category' ),
-        'menu_name'         => __( 'Service Categories' ),
+        'name'              => _x( 'Treatment Categories', 'taxonomy general name' ),
+        'singular_name'     => _x( 'Treatment Category', 'taxonomy singular name' ),
+        'search_items'      => __( 'Search Treatment Categories' ),
+        'all_items'         => __( 'All Treatment Categories' ),
+        'edit_item'         => __( 'Edit Treatment Category' ),
+        'update_item'       => __( 'Update Treatment Category' ),
+        'add_new_item'      => __( 'Add New Treatment Category' ),
+        'new_item_name'     => __( 'New Tile Treatment Category' ),
+        'menu_name'         => __( 'Treatment Categories' ),
     );
 
     $args = array(
@@ -46,10 +46,10 @@ function  act_cpt() {
         'show_ui'           => true,
         'show_admin_column' => true,
         'query_var'         => true,
-        'rewrite'           => array( 'slug' => 'service-category' ),
+        'rewrite'           => array( 'slug' => 'treatment-category' ),
     );
 
-    register_taxonomy( 'service_category', array( 'service' ), $args );
+    register_taxonomy( 'treatment_category', array( 'treatment' ), $args );
 
 }
 
