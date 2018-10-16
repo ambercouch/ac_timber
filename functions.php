91614b049475c8d4e456dcd452a9d536f920a72e
Richiearnold <richard@ambercouch.co.uk>
2018-10-16 19:58:32 +0100

    add AC admin styles

--- functions.php
+++ functions.php
@@ -86,3 +86,9 @@ require_once get_template_directory() . '/lib/functions--walkers.php';
 //}
 //// Attach callback to 'tiny_mce_before_init'
 //add_filter( 'tiny_mce_before_init', 'my_custom_styles' );
+
+function my_admin_theme_style() {
+    wp_enqueue_style('ac-admin-theme', get_stylesheet_directory_uri().'/assets/css/ac-admin.css');
+}
+add_action('admin_enqueue_scripts', 'my_admin_theme_style');
+add_action('login_enqueue_scripts', 'my_admin_theme_style');
