cd50e169eff224eacc3b50ae3440ead80e62455b
Richie Arnold <richard@ambercouch.co.uk>
2018-10-24 12:16:27 +0100

    Added Event post type and location custom fields

--- lib/functions--cpt.php
+++ lib/functions--cpt.php
@@ -2,42 +2,42 @@
 function  act_cpt() {
 //Tiles
     $labels = array(
-        'name' => _x('Services', 'post type general name'),
-        'singular_name' => _x('Service', 'post type singular name'),
-        'add_new' => _x('Add New', 'Service'),
-        'add_new_item' => __('Add New Service'),
-        'edit_item' => __('Edit Service'),
-        'new_item' => __('New Service'),
-        'all_items' => __('All Services'),
-        'view_item' => __('View Service'),
-        'search_items' => __('Search Services'),
-        'not_found' => __('No Services found'),
-        'not_found_in_trash' => __('No Services found in the trash'),
+        'name' => _x('Events', 'post type general name'),
+        'singular_name' => _x('Event', 'post type singular name'),
+        'add_new' => _x('Add New', 'Event'),
+        'add_new_item' => __('Add New Event'),
+        'edit_item' => __('Edit Event'),
+        'new_item' => __('New Event'),
+        'all_items' => __('All Events'),
+        'view_item' => __('View Event'),
+        'search_items' => __('Search Events'),
+        'not_found' => __('No Events found'),
+        'not_found_in_trash' => __('No Events found in the trash'),
         'parent_item_colon' => '',
-        'menu_name' => 'Services'
+        'menu_name' => 'Events'
     );
     $args = array(
         'labels' => $labels,
-        'menu_icon' => 'dashicons-store',
-        'description' => 'Services offered',
+        'menu_icon' => 'dashicons-format-chat',
+        'description' => 'Events offered',
         'public' => true,
         'menu_position' => 20,
         'supports' => array('title','editor','author','thumbnail','excerpt','trackbacks','custom-fields','comments','revisions','page-attributes','post-formats'),
-        'has_archive' => 'service'
+        'has_archive' => 'event'
     );
-    register_post_type('service', $args);
+    register_post_type('event', $args);
 
-//Service Categories
+//Event Categories
     $labels = array(
-        'name'              => _x( 'Service Categories', 'taxonomy general name' ),
-        'singular_name'     => _x( 'Service Category', 'taxonomy singular name' ),
-        'search_items'      => __( 'Search Service Categories' ),
-        'all_items'         => __( 'All Service Categories' ),
-        'edit_item'         => __( 'Edit Service Category' ),
-        'update_item'       => __( 'Update Service Category' ),
-        'add_new_item'      => __( 'Add New Service Category' ),
-        'new_item_name'     => __( 'New Tile Service Category' ),
-        'menu_name'         => __( 'Service Categories' ),
+        'name'              => _x( 'Event Categories', 'taxonomy general name' ),
+        'singular_name'     => _x( 'Event Category', 'taxonomy singular name' ),
+        'search_items'      => __( 'Search Event Categories' ),
+        'all_items'         => __( 'All Event Categories' ),
+        'edit_item'         => __( 'Edit Event Category' ),
+        'update_item'       => __( 'Update Event Category' ),
+        'add_new_item'      => __( 'Add New Event Category' ),
+        'new_item_name'     => __( 'New Tile Event Category' ),
+        'menu_name'         => __( 'Event Categories' ),
     );
 
     $args = array(
@@ -46,10 +46,34 @@ function  act_cpt() {
         'show_ui'           => true,
         'show_admin_column' => true,
         'query_var'         => true,
-        'rewrite'           => array( 'slug' => 'service-category' ),
+        'rewrite'           => array( 'slug' => 'event-category' ),
     );
 
-    register_taxonomy( 'service_category', array( 'service' ), $args );
+    register_taxonomy( 'event_category', array( 'event' ), $args );
+
+    //Event Locations
+    $labels = array(
+        'name'              => _x( 'Event Locations', 'taxonomy general name' ),
+        'singular_name'     => _x( 'Event Location', 'taxonomy singular name' ),
+        'search_items'      => __( 'Search Event Locations' ),
+        'all_items'         => __( 'All Event Locations' ),
+        'edit_item'         => __( 'Edit Event Location' ),
+        'update_item'       => __( 'Update Event Location' ),
+        'add_new_item'      => __( 'Add New Event Location' ),
+        'new_item_name'     => __( 'New Tile Event Location' ),
+        'menu_name'         => __( 'Event Locations' ),
+    );
+
+    $args = array(
+        'hierarchical'      => false,
+        'labels'            => $labels,
+        'show_ui'           => true,
+        'show_admin_column' => true,
+        'query_var'         => true,
+        'rewrite'           => array( 'slug' => 'event-location' ),
+    );
+
+    register_taxonomy( 'event_location', array( 'event' ), $args );
 
 }
 
