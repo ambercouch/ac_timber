2a0957460f0098e549f070e84b1d41cccd288fa9
Richiearnold <richard@ambercouch.co.uk>
2019-05-06 12:37:20 +0100

    Update setting with typekit options

--- lib/functions--ac-settings.php
+++ lib/functions--ac-settings.php
@@ -5,7 +5,7 @@
     {
         $settings['svgLogo'] = null;
         $settings['defaultImage'] = null;
-        $settings['typeKitId'] = 'hej7ovh';
+        $settings['typeKitId'] = (get_field('typekit_id', 'options') != '' )? get_field('typekit_id', 'options') : 'uga0lgj';
         $settings['sidebars'] = unserialize(ACT_SIDEBARS);
         $settings['bannerImg']['url'] = get_stylesheet_directory_uri().'/assets/images/jpg/act-default.jpg';
         $settings['bannerImg']['alt'] = 'AmberCouch Timber Theme';
