aa0716226fe19a1aea0f6696ce2fd0455b6cd349
Richiearnold <richard@ambercouch.co.uk>
2019-05-08 12:19:25 +0100

    Update nav menu to fix error

--- lib/wp-timber/timber--nav-menu.php
+++ lib/wp-timber/timber--nav-menu.php
@@ -2,6 +2,14 @@
 
 function act_nav_menu_walker( $args ) {
 
+    //Test the $args to get the slug
+    $slug = '';
+    if (is_object($args['menu'])){
+        $slug = $args['menu']->slug;
+    }
+    elseif (is_string($args['menu'])){
+        $slug = strtolower($args['menu']);
+    }
 
     return array_merge( $args, array(
         'walker' => new Ac_Walker_Nav_Menu(),
