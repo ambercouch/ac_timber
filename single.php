40a0419344bc15e6871e017001dbec412a61d222
Richie Arnold <richard@ambercouch.co.uk>
2019-05-26 13:16:15 +0100

    Add image quote template and styles

--- single.php
+++ single.php
@@ -15,6 +15,23 @@ $context['post'] = $post;
 $context['template'] = $post->post_type;
 $context['postType'] = $post->post_type;
 
+//Page Quoutes show a testiomnial on a case study page
+$pageQuote = new TimberPost(get_field('page_testimonial'));
+
+$quoteImg = get_field('quote_background_image', $pageQuote->ID);
+
+$quoteText = get_the_excerpt($pageQuote->ID);
+$quoteName = get_field('citation_name', $pageQuote->ID);
+$quoteInfo = get_field('citation_info', $pageQuote->ID);
+
+$context['pageQuote'] = array(
+    'quoteImg' => $quoteImg,
+    'quoteText' => $quoteText,
+    'quoteName' => $quoteName,
+    'quoteInfo' => $quoteInfo
+);
+
+//ac_dd($quoteImg);
 
 require_once get_template_directory() . '/lib/wp-timber/timber--comment-form.php';
 
