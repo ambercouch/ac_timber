65378bee91b0c9bb7023c9f5b7198b385669a5c1
Richie Arnold <richard@ambercouch.co.uk>
2019-09-20 19:38:55 +0100

    Added page banner setting

---
+++ lib/acf/functions--acf-page-banner-content.php
@@ -0,0 +1,78 @@
+<?php
+
+if( function_exists('acf_add_local_field_group') ):
+
+    acf_add_local_field_group(array (
+        'key' => 'group_page_banner_content',
+        'title' => 'Page Banner Content',
+        'fields' => array (
+            array (
+                'key' => 'field_page_banner_content',
+                'label' => 'Page Banner Content',
+                'name' => 'page_banner_content',
+                'type' => 'wysiwyg',
+                'instructions' => 'Add text and images here to appear on the hero banner',
+                'required' => 0,
+                'conditional_logic' => 0,
+                'wrapper' => array (
+                    'width' => '',
+                    'class' => '',
+                    'id' => '',
+                ),
+                'default_value' => '',
+                'tabs' => 'all',
+                'toolbar' => 'full',
+                'media_upload' => 1,
+            ),
+            array (
+                'key' => 'field_page_banner_logo',
+                'label' => 'Page Banner Logo',
+                'name' => 'page_banner_logo',
+                'type' => 'image',
+                'instructions' => '',
+                'required' => 0,
+                'conditional_logic' => 0,
+                'wrapper' => array (
+                    'width' => '',
+                    'class' => '',
+                    'id' => '',
+                ),
+                'return_format' => 'array',
+                'preview_size' => 'thumbnail',
+                'library' => 'all',
+                'min_width' => '',
+                'min_height' => '',
+                'min_size' => '',
+                'max_width' => '',
+                'max_height' => '',
+                'max_size' => '',
+                'mime_types' => '',
+            ),
+        ),
+        'location' => array (
+            array (
+                array (
+                    'param' => 'post_type',
+                    'operator' => '==',
+                    'value' => 'page',
+                ),
+            ),
+            array (
+                array (
+                    'param' => 'post_type',
+                    'operator' => '==',
+                    'value' => 'service',
+                ),
+            ),
+        ),
+        'menu_order' => 2,
+        'position' => 'normal',
+        'style' => 'default',
+        'label_placement' => 'top',
+        'instruction_placement' => 'label',
+        'hide_on_screen' => '',
+        'active' => 1,
+        'description' => '',
+    ));
+
+endif;
