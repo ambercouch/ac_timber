9bdb3370d213ae653aaf9e1b8d2642c3f9b74f88
Richie Arnold <richard@ambercouch.co.uk>
2019-11-18 08:53:02 +0000

    Add menu icon position functions

--- lib/functions--ac-menu-filters.php
+++ lib/functions--ac-menu-filters.php
@@ -19,12 +19,33 @@ function act_menu_filters($sorted_menu_objects, $args) {
 
     //icon menus
     foreach ($sorted_menu_objects as $menu_object) {
+       // print_r($menu_object);
 
         $menu_item_icon = get_field('menu_icon', $menu_object);
+        $menu_icon_position = get_field('icon_position', $menu_object);
+        $menu_show_label = get_field('show_label', $menu_object);
+
+        $menu_label = $menu_object->title;
+        $menu_label_markup = '';
+        $menu_icon_markup = '';
+
+
 
         if ($menu_item_icon != ""){
             $icon_class_mod = str_replace("icon-","", $menu_item_icon);
             $menu_item_icon_id = "#".$menu_item_icon;
+
+            $menu_icon_markup =  '<div class="c-nav-menu__svg-icon"><div class="c-svg-icon--'. $icon_class_mod .'"><svg preserveAspectRatio="none" class="c-svg-icon__svg"><use class="c-svg-icon__use"  xlink:href="' . $menu_item_icon_id  . '" /></svg></div></div>';
+
+            if($menu_show_label == 1){
+                $menu_label_class =  ($menu_icon_position == 'left') ? 'c-nav-menu__link-label--svg-icon-left' : 'c-nav-menu__link-label--svg-icon-right';
+                $menu_label_markup = '<div class="'.$menu_label_class.'">'.$menu_label.'</div>';
+                $menu_object->classes[] = ($menu_icon_position == 'left') ? 'c-nav-menu__item--svg-icon-left' : 'c-nav-menu__item--svg-icon-right';
+            }else{
+                $menu_object->classes[] = 'c-nav-menu__item--svg-icon';
+            }
+
+
 
             $title_markup = false;
             if ($menu_object->attr_title != ""){
@@ -32,7 +53,13 @@ function act_menu_filters($sorted_menu_objects, $args) {
                 $title_markup = '<div class="c-nav-menu__icon-title">'.$attr_title.'</div>';
             }
 
-            $menu_object->title = '<div class="c-nav-menu__svg-icon"><div class="c-svg-icon--'. $icon_class_mod .'"><svg preserveAspectRatio="none" class="c-svg-icon__svg"><use class="c-svg-icon__use"  xlink:href="' . $menu_item_icon_id  . '" /></svg></div></div>'.$title_markup;
+            $menu_object->title = '';
+            if($menu_icon_position == 'left'){
+                $menu_object->title = $menu_icon_markup.$menu_label_markup;
+            }else{
+                $menu_object->title = $menu_label_markup. $menu_icon_markup;
+            }
+
 
         }
     }
