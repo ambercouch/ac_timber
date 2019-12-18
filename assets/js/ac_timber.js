bcb16446332aeae80ea75c303e4c2d4a2560ddcd
Richie Arnold <richard@ambercouch.co.uk>
2019-12-18 08:16:20 +0000

    Add you tube lazy loader

--- assets/js/ac_timber.js
+++ assets/js/ac_timber.js
@@ -9,7 +9,20 @@ ACTIMBER = {
             'use strict';
             //uncomment to debug
             fitvids()
-            console.log('common 123 fitvids');
+            console.log('common 123 youtube');
+
+            var video_wrapper = $('.youtube-video-place');
+            //  Check to see if youtube wrapper exists
+
+            if(video_wrapper.length){
+                // If user clicks on the video wrapper load the video.
+                $('.play-youtube-video').on('click', function(){
+                    /* Dynamically inject the iframe on demand of the user.
+                     Pull the youtube url from the data attribute on the wrapper element. */
+                    video_wrapper.html('<iframe allowfullscreen frameborder="0" class="embed-responsive-item" src="' + video_wrapper.data('yt-url') + '"></iframe>');
+                    fitvids();
+                });
+            }
 
             //add js class
             //jQuery('body').addClass('js');
