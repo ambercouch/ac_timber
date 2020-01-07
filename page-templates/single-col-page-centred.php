b457cb7f2f7256eab4e8d57638aa6790a6c2ce17
Richie Arnold <richard@ambercouch.co.uk>
2020-01-07 10:14:16 +0000

    Add single-page-centred page template and styles

---
+++ page-templates/single-col-page-centred.php
@@ -0,0 +1,21 @@
+<?php /** Template Name: Single Col Page Centred
+ *
+ * @package WordPress
+ * @subpackage ac_timber
+ */
+$templateSlug = 'single-col-page-centred';
+$context = Timber::get_context();
+$post = new TimberPost();
+$context['post'] = $post;
+$context['primary_widgets'] = Timber::get_widgets('Primary');
+$context['footer_widgets'] = Timber::get_widgets('Footer');
+$context['template'] = $templateSlug;
+
+//require_once get_template_directory() . '/lib/wp-timber/functions/timber--comment-form.php';
+$templates = array( $templateSlug.'.twig', 'page.twig' );
+
+if ( post_password_required( $post->ID ) ) {
+    Timber::render( 'single-password.twig', $context );
+} else {
+    Timber::render( $templates, $context );
+}
