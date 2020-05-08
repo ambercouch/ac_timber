767f59cbe4f81b1c0b60a988fb549b53a08535fe
Richard Arnold <richard@ambercouch.co.uk>
2020-05-08 12:55:17 +0100

    Update homepage styles and templates

--- loop-template.php
+++ loop-template.php
@@ -6,6 +6,14 @@
  * Time: 13:46
  */
 
+$excerpt = false;
+
+if (has_excerpt()){
+    $excerpt = get_the_excerpt();
+}elseif (get_field('page_intro')){
+  $excerpt = get_field('page_intro');
+}
+
 $handle = 'ac_wp_custom_loop_styles';
 $list = 'enqueued';
 
@@ -15,25 +23,28 @@ if (! wp_script_is( $handle, $list )) {
 }
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class('c-accl-post-list__post-thumb '); ?>>
-  <div class="c-accl-post-thumb">
+<article id="post-<?php the_ID(); ?>" <?php post_class('l-post-thumb-list__item--post-thumb'); ?>>
+  <div class="c-post-thumb">
+    <header  class="c-post-thumb__header">
+      <h2 class="c-post-thumb__heading">
+             <span class="c-post-thumb__title">
+             <a href="<?php echo esc_url( get_permalink() ) ?>" class="c-post-thumb__link--title" rel="bookmark">
+               <span class="c-post-thumb__link-title"><?php the_title() ?></span>
+             </a>
+               </span>
+      </h2>
+    </header>
     <?php if ( '' !== get_the_post_thumbnail() ) : ?>
-      <div class="post-thumbnail c-accl-post-thumb__feature-image">
-        <a href="<?php the_permalink(); ?>" class="c-accl-post-thumb__feature-image-link" >
+      <div class="c-post-thumb__feature-image">
+        <a href="<?php the_permalink(); ?>" class="c-post-thumb__link--image" >
             <?php the_post_thumbnail('post-thumbnail'); ?>
         </a>
       </div><!-- .post-thumbnail -->
     <?php endif; ?>
-         <header  class="entry-header c-accl-post-thumb__header">
-           <h2 class="entry-title c-accl-post-thumb__heading">
-             <a href="<?php esc_url( get_permalink() ) ?>" class="c-accl-post-thumb__link" rel="bookmark">
-               <span class="c-accl-post-thumb__link-title"><?php the_title() ?></span>
-             </a>
-           </h2>
-         </header>
-    <?php if (has_excerpt()) : ?>
-      <div class="c-accl-post-thumb__excerpt"  >
-        <?php the_excerpt() ?>
+
+    <?php if ($excerpt) : ?>
+      <div class="c-post-thumb__excerpt"  >
+        <?php echo $excerpt ?>
       </div>
     <?php endif; ?>
   </div>
