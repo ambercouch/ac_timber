47ab0b7cdc0d2fa80871f65ec1f9855c1967e461
Richard Arnold <richard@ambercouch.co.uk>
2020-05-19 10:04:46 +0100

    Update c-widget-area styles and templates to form component to layouts

--- lib/functions--widget-areas.php
+++ lib/functions--widget-areas.php
@@ -11,7 +11,7 @@ function _act_widgets_init() {
             'name' => __($sidebar , '_act'),
             'id' => 'aside-'.$sidebar_slug,
             'description' => '',
-            'before_widget' => '<aside id="%1$s" class="c-widget-area__widget--'.$sidebar_slug.'-widgets %2$s"><div  class="widget c-widget--'.$sidebar_slug.'">',
+            'before_widget' => '<aside id="%1$s" class="l-widget-area__widget--'.$sidebar_slug.'-widgets %2$s"><div  class="widget c-widget--'.$sidebar_slug.'">',
             'after_widget' => '</div></aside>',
             'before_title' => '<h3 class="c-widget__heading--'.$sidebar_slug  . '" ><span class="c-widget__title">',
             'after_title' => '</span></h3>'
