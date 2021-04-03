f673e62bacb7a4652e8e75a14bcd7684368d42f0
Richie Arnold <richard@ambercouch.co.uk>
2021-04-03 17:24:07 +0100

    ACT - Add custom post type project

--- lib/functions--cpt.php
+++ lib/functions--cpt.php
@@ -1,5 +1,29 @@
 <?php
 function  act_cpt() {
+    $labels = array(
+        'name' => _x('Projects', 'post type general name'),
+        'singular_name' => _x('Project', 'post type singular name'),
+        'add_new' => _x('Add New', 'project'),
+        'add_new_item' => __('Add New Project'),
+        'edit_item' => __('Edit Project'),
+        'new_item' => __('New Project'),
+        'all_items' => __('All Projects'),
+        'view_item' => __('View Project'),
+        'search_items' => __('Search Projects'),
+        'not_found' => __('No projects found'),
+        'not_found_in_trash' => __('No projects found in the Trash'),
+        'parent_item_colon' => '',
+        'menu_name' => 'Projects'
+    );
+    $args = array(
+        'labels' => $labels,
+        'description' => 'Ambercouch Projects',
+        'public' => true,
+        'menu_position' => 5,
+        'supports' => array('title', 'editor', 'thumbnail', 'excerpt', 'comments','page-attributes' ),
+        'has_archive' => true,
+    );
+    register_post_type('project', $args);
 //Services
     $labels = array(
         'name' => _x('Services', 'post type general name'),
