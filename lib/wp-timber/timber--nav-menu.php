3af064f66c8c100b7d4e6194086d84b55ac43679
Richie Arnold <richard@ambercouch.co.uk>
2021-04-21 09:57:30 +0100

    PHX - Update nav menu

--- lib/wp-timber/timber--nav-menu.php
+++ lib/wp-timber/timber--nav-menu.php
@@ -2,11 +2,18 @@
 
 function act_nav_menu_walker( $args ) {
 
-
+    //Test the $args to get the slug
+    $slug = '';
+    if (is_object($args['menu'])){
+        $slug = $args['menu']->slug;
+    }
+    elseif (is_string($args['menu'])){
+        $slug = str_replace(' ', '-', strtolower($args['menu']));
+    }
     return array_merge( $args, array(
         'walker' => new Ac_Walker_Nav_Menu(),
-        'menu_class' => 'c-nav-menu__list--'.$args['menu']->slug,
-        'container_class' => 'c-nav-menu--'.$args['menu']->slug
+        'menu_class' => 'c-nav-menu__list--'.$slug,
+        'container_class' => 'c-nav-menu--'.$slug
     ) );
 }
 add_filter( 'wp_nav_menu_args', 'act_nav_menu_walker' );
