4e8250d5f38777e109fa1487b8a0e8da3fe4b083
Richie Arnold <richard@ambercouch.co.uk>
2021-06-28 09:57:46 +0100

    Add Galley item post type

--- lib/functions--cpt.php
+++ lib/functions--cpt.php
@@ -1,79 +1,52 @@
 <?php
 function  act_cpt() {
     $labels = array(
-        'name' => _x('Projects', 'post type general name'),
-        'singular_name' => _x('Project', 'post type singular name'),
-        'add_new' => _x('Add New', 'project'),
-        'add_new_item' => __('Add New Project'),
-        'edit_item' => __('Edit Project'),
-        'new_item' => __('New Project'),
-        'all_items' => __('All Projects'),
-        'view_item' => __('View Project'),
-        'search_items' => __('Search Projects'),
-        'not_found' => __('No projects found'),
-        'not_found_in_trash' => __('No projects found in the Trash'),
+        'name' => _x('Gallery Items', 'post type general name'),
+        'singular_name' => _x('Gallery Item', 'post type singular name'),
+        'add_new' => _x('Add New', 'gallery item'),
+        'add_new_item' => __('Add New Gallery Item'),
+        'edit_item' => __('Edit Gallery Item'),
+        'new_item' => __('New Gallery Item'),
+        'all_items' => __('All Gallery Items'),
+        'view_item' => __('View Gallery Item'),
+        'search_items' => __('Search Gallery Items'),
+        'not_found' => __('No gallery items found'),
+        'not_found_in_trash' => __('No gallery items found in the Trash'),
         'parent_item_colon' => '',
-        'menu_name' => 'Projects'
+        'menu_name' => 'Gallery'
     );
     $args = array(
         'labels' => $labels,
-        'description' => 'Ambercouch Projects',
+        'description' => 'Cov19chronicles art work',
         'public' => true,
         'menu_position' => 5,
         'supports' => array('title', 'editor', 'thumbnail', 'excerpt', 'comments','page-attributes' ),
         'has_archive' => true,
     );
-    register_post_type('project', $args);
-//Services
-    $labels = array(
-        'name' => _x('Services', 'post type general name'),
-        'singular_name' => _x('Service', 'post type singular name'),
-        'add_new' => _x('Add New', 'Service'),
-        'add_new_item' => __('Add New Service'),
-        'edit_item' => __('Edit Service'),
-        'new_item' => __('New Service'),
-        'all_items' => __('All Services'),
-        'view_item' => __('View Service'),
-        'search_items' => __('Search Services'),
-        'not_found' => __('No Services found'),
-        'not_found_in_trash' => __('No Services found in the trash'),
-        'parent_item_colon' => '',
-        'menu_name' => 'Services'
-    );
-    $args = array(
-        'labels' => $labels,
-        'menu_icon' => 'dashicons-store',
-        'description' => 'Services offered',
-        'public' => true,
-        'menu_position' => 20,
-        'supports' => array('title','editor','author','thumbnail','excerpt','trackbacks','custom-fields','comments','revisions','page-attributes','post-formats'),
-        'has_archive' => 'service'
-    );
-    register_post_type('service', $args);
+    register_post_type('gallery-item', $args);
 
-//Service Categories
     $labels = array(
-        'name'              => _x( 'Service Categories', 'taxonomy general name' ),
-        'singular_name'     => _x( 'Service Category', 'taxonomy singular name' ),
-        'search_items'      => __( 'Search Service Categories' ),
-        'all_items'         => __( 'All Service Categories' ),
-        'edit_item'         => __( 'Edit Service Category' ),
-        'update_item'       => __( 'Update Service Category' ),
-        'add_new_item'      => __( 'Add New Service Category' ),
-        'new_item_name'     => __( 'New Tile Service Category' ),
-        'menu_name'         => __( 'Service Categories' ),
+        'name'              => _x( 'Item Type', 'taxonomy general name' ),
+        'singular_name'     => _x( 'Item Type', 'taxonomy singular name' ),
+        'search_items'      => __( 'Search Item Types' ),
+        'all_items'         => __( 'All Item Types' ),
+        'edit_item'         => __( 'Edit Item Type' ),
+        'update_item'       => __( 'Update Item Type' ),
+        'add_new_item'      => __( 'Add New Item Type' ),
+        'new_item_name'     => __( 'New Item Type' ),
+        'menu_name'         => __( 'Gallery Item Types' ),
     );
 
     $args = array(
-        'hierarchical'      => false,
+        'hierarchical'      => true,
         'labels'            => $labels,
         'show_ui'           => true,
         'show_admin_column' => true,
         'query_var'         => true,
-        'rewrite'           => array( 'slug' => 'service-category' ),
+        'rewrite'           => array( 'slug' => 'gallery-type' ),
     );
 
-    register_taxonomy( 'service_category', array( 'service' ), $args );
+    register_taxonomy( 'gallery_type', array( 'gallery-item' ), $args );
 
 //    $labels = array(
 //        'name' => _x('Testimonials', 'post type general name'),
