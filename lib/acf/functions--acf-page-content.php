9fa255d15ac64355088473d9d53b5b5c2fc1c11d
Richie Arnold <richard@ambercouch.co.uk>
2021-10-29 08:06:23 +0100

    ACT - Add additional custom fields to page conent

--- lib/acf/functions--acf-page-content.php
+++ lib/acf/functions--acf-page-content.php
@@ -85,3 +85,83 @@ if( function_exists('acf_add_local_field_group') ):
     ));
 
 endif;
+
+if( function_exists('acf_add_local_field_group') ):
+
+    acf_add_local_field_group(array(
+        'key' => 'group_post_content',
+        'title' => 'Post Content',
+        'fields' => array(
+            array(
+                'key' => 'field_content_aside',
+                'label' => 'Post Content Aside',
+                'name' => 'post_content_aside',
+                'type' => 'wysiwyg',
+                'instructions' => 'Extra content area use as a sidebar',
+                'required' => 0,
+                'conditional_logic' => 0,
+                'wrapper' => array(
+                    'width' => '50',
+                    'class' => '',
+                    'id' => '',
+                ),
+                'default_value' => '',
+                'tabs' => 'all',
+                'toolbar' => 'full',
+                'media_upload' => 1,
+                'delay' => 0,
+            ),
+            array(
+                'key' => 'field_footer_content',
+                'label' => 'Post Footer Content',
+                'name' => 'post_footer_content',
+                'type' => 'wysiwyg',
+                'instructions' => 'Extra content area used at the bottom of the article.',
+                'required' => 0,
+                'conditional_logic' => 0,
+                'wrapper' => array(
+                    'width' => '50',
+                    'class' => '',
+                    'id' => '',
+                ),
+                'default_value' => '',
+                'tabs' => 'all',
+                'toolbar' => 'full',
+                'media_upload' => 1,
+                'delay' => 0,
+            ),
+        ),
+        'location' => array(
+            array(
+                array(
+                    'param' => 'post_type',
+                    'operator' => '==',
+                    'value' => 'service',
+                ),
+            ),
+            array(
+                array(
+                    'param' => 'post_type',
+                    'operator' => '==',
+                    'value' => 'page',
+                ),
+            ),
+            array(
+                array(
+                    'param' => 'post_type',
+                    'operator' => '==',
+                    'value' => 'post',
+                ),
+            ),
+        ),
+        'menu_order' => 0,
+        'position' => 'normal',
+        'style' => 'default',
+        'label_placement' => 'top',
+        'instruction_placement' => 'label',
+        'hide_on_screen' => '',
+        'active' => true,
+        'description' => '',
+    ));
+
+endif;
