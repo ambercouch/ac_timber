016ec91a2b5db5e72d2b595d200fdd6dc79a8476
Richie Arnold <richard@ambercouch.co.uk>
2022-06-29 10:47:17 +0100

    ACT - Update post slug

--- lib/functions--cpt.php
+++ lib/functions--cpt.php
@@ -25,7 +25,7 @@ function  act_cpt() {
         'show_in_rest' => true,
         'hierarchical' => true,
         'has_archive' => false,
-        'rewrite'   => array( 'slug' => '/', 'with_front' => false ),
+        //'rewrite'   => array( 'slug' => '/', 'with_front' => true ),
         'supports' => array('title','editor','author','thumbnail','excerpt','custom-fields','revisions','page-attributes'),
     );
     register_post_type('service', $args);
@@ -83,3 +83,54 @@ function  act_cpt() {
 }
 
 add_action('init', 'act_cpt');
+
+/**
+ * Remove the slug from published post permalinks. Only affect our custom post type, though.
+ */
+function ac_remove_cpt_slug( $post_link, $post ) {
+
+    if ( 'service' === $post->post_type && 'publish' === $post->post_status ) {
+        $post_link = str_replace( '/' . $post->post_type . '/', '/', $post_link );
+    }
+
+    return $post_link;
+}
+add_filter( 'post_type_link', 'ac_remove_cpt_slug', 10, 2 );
+
+/**
+ * Have WordPress match postname to any of our public post types (post, page, race).
+ * All of our public post types can have /post-name/ as the slug, so they need to be unique across all posts.
+ * By default, WordPress only accounts for posts and pages where the slug is /post-name/.
+ *
+ * @param $query The current query.
+ */
+function ac_add_cpt_post_names_to_main_query( $query ) {
+
+    if ( $query->is_main_query() ) {
+
+        //return;
+    }
+
+    // Bail if this is not the main query.
+    if ( ! $query->is_main_query() ) {
+        return;
+    }
+
+    // Bail if this query doesn't match our very specific rewrite rule.
+    if ( (! isset( $query->query['page'] ) ||
+            2 !== count( $query->query ) ) &&
+        (! isset( $query->query['attachment'] )
+        ) ) {
+
+        return;
+    }
+
+    // Bail if we're not querying based on the post name.
+    if ( empty( $query->query['name'] )  &&  ! isset( $query->query['attachment'] ) ) {
+        return;
+    }
+
+    // Add CPT to the list of post types WP will include when it queries based on the post name.
+    $query->set( 'post_type', array( 'post', 'page', 'service' ) );
+}
+add_action( 'pre_get_posts', 'ac_add_cpt_post_names_to_main_query' );
