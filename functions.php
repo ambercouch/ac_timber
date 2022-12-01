1436311c5b81e0bed72ab19ebcd66ade6137cd76
Richie Arnold <richard@ambercouch.co.uk>
2022-12-01 13:55:59 +0000

    ACT - Update admin enque

--- functions.php
+++ functions.php
@@ -94,7 +94,7 @@ require_once get_template_directory() . '/lib/functions--walkers.php';
 //add_filter( 'tiny_mce_before_init', 'my_custom_styles' );
 
 function my_admin_theme_style() {
-    wp_enqueue_style('ac-admin-theme', get_stylesheet_directory_uri().'/assets/css/ac-admin.css');
+    wp_enqueue_style('ac-admin-theme', get_stylesheet_directory_uri().'/assets/css/ac-admin.css', array(), filemtime(get_template_directory() . '/assets/css/ac-admin.css'));
 }
 add_action('admin_enqueue_scripts', 'my_admin_theme_style');
 add_action('login_enqueue_scripts', 'my_admin_theme_style');
