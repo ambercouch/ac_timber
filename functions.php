7b1b70e47f7fb7b0b39c3a66d83b8e6ae00bf521
Richie Arnold <richard@ambercouch.co.uk>
2023-06-13 13:12:18 +0100

    ACT - Add cf7 auto p false

--- functions.php
+++ functions.php
@@ -95,3 +95,60 @@ function my_admin_theme_style() {
 }
 add_action('admin_enqueue_scripts', 'my_admin_theme_style');
 add_action('login_enqueue_scripts', 'my_admin_theme_style');
+
+
+function act_disable_editor( $id = false ) {
+
+    $excluded_templates = array(
+        //'page-templates/mxb-home.php',
+    );
+
+    $excluded_ids = array(
+        //get_option( 'page_on_front' )
+    );
+
+    if( empty( $id ) )
+        return false;
+
+    $id = intval( $id );
+    $template = get_page_template_slug( $id );
+
+    return in_array( $id, $excluded_ids ) || in_array( $template, $excluded_templates );
+}
+
+/**
+ * Disable Gutenberg by template
+ *
+ */
+function act_disable_gutenberg( $can_edit, $post_type ) {
+
+    if( ! ( is_admin() && !empty( $_GET['post'] ) ) )
+        return $can_edit;
+
+    if( act_disable_editor( $_GET['post'] ) )
+        $can_edit = false;
+
+    return $can_edit;
+
+}
+add_filter( 'gutenberg_can_edit_post_type', 'act_disable_gutenberg', 10, 2 );
+add_filter( 'use_block_editor_for_post_type', 'act_disable_gutenberg', 10, 2 );
+
+/**
+ * Disable Classic Editor by template
+ *
+ */
+function act_disable_classic_editor() {
+
+    $screen = get_current_screen();
+    if( 'page' !== $screen->id || ! isset( $_GET['post']) )
+        return;
+
+    if( act_disable_editor( $_GET['post'] ) ) {
+        remove_post_type_support( 'page', 'editor' );
+    }
+
+}
+add_action( 'admin_head', 'act_disable_classic_editor' );
+
+add_filter('wpcf7_autop_or_not', '__return_false');
