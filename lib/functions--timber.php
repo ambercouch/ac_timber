750112f71bb0b8507c4602cf63387f3b9d7a9ca4
Richie Arnold <richard@ambercouch.co.uk>
2023-07-15 16:32:52 +0100

    ACT - Update environment var

--- lib/functions--timber.php
+++ lib/functions--timber.php
@@ -79,7 +79,13 @@ class StarterSite extends TimberSite {
 
         //Site vars
         $context['site'] = $this;
-        $context['environment'] = ENVIRONMENT;
+
+        //Check if the ENVIRONMENT constant is defined
+        if(defined('ENVIRONMENT')){
+            $context['environment'] = ENVIRONMENT;
+        }else{
+            $context['environment'] = 'production';
+        }
 
         //AC Template settings
         $context['acSettings'] = acSettings();
