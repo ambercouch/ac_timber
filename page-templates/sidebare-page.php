e343ab5124088f102946013c897bf3eec1c24f41
Richie Arnold <richard@ambercouch.co.uk>
2024-05-13 15:18:01 +0100

    SHL - Update sidebar template

--- page-templates/sidebare-page.php
+++ page-templates/sidebare-page.php
@@ -17,54 +17,6 @@ $context['mainMod'] =  '--page-sidebar';
 $context['template'] = $templateSlug;
 $templates = array( 'page-sidebar-' . $post->post_name . '.twig', 'page-sidebar.twig', 'page.twig' );
 
-//get childlist
-$children = false;
-
-if(!$WPPost->post_parent){
-    $args = array(
-        'title_li' => '',
-        'child_of' => $post->ID,
-        'echo' => 0,
-    );
-    $children = wp_list_pages($args);
-}else{
-    if($WPPost->ancestors)
-    {
-        $ancestors = $WPPost->ancestors[sizeof($WPPost->ancestors) - 1];
-        $args = array(
-            'title_li' => '',
-            'child_of' => $ancestors,
-            'echo' => 0,
-        );
-        $children = wp_list_pages($args);
-        $parentId = ($ancestors == 11)? '158' : $ancestors;
-        $context['parentId'] = $parentId    ;
-        $parent_link = get_permalink($parentId);
-        $context['parentLink'] = $parent_link;
-    }
-}
-
-$context['childList'] = $children;
-
-//Ancestor Logic
-if( is_page() && $post->post_parent > 0 ) {
-    $children = get_pages('child_of='.$post->ID);
-    if( count( $children ) != 0 ) {
-        //Has Child
-    }
-    $parent = get_post_ancestors($post->ID);
-
-    if( count( $children ) <= 0  && empty($parent[1]) ) {
-        // Is child
-
-    } elseif ( count( $children ) <= 0  && !empty($parent[1]) )  {
-        // Is child of child
-    }
-} else {
-    //Barren Orphan :(
-}
-
-
 if ( post_password_required( $post->ID ) ) {
     Timber::render( 'single-password.twig', $context );
 } else {
