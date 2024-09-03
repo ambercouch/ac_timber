7a4a71a752d1c0b3acd52eca4a8b9986dd24c260
Richie Arnold <richard@ambercouch.co.uk>
2024-09-03 11:05:22 +0100

    WDW - Added single season templates and styles

--- single.php
+++ single.php
@@ -8,7 +8,6 @@
  * @subpackage  Timber
  * @since    Timber 0.1
  */
-
 $context = Timber::get_context();
 $post = Timber::query_post();
 $context['post'] = $post;
@@ -16,7 +15,12 @@ $context['mainMod'] = '--'.$post->post_type;
 $context['contentMod'] = '--'.$post->post_type;
 $context['template'] = $post->post_type;
 $context['postType'] = $post->post_type;
-
+$season_content = get_field('season_content', $post->ID);
+$context['season_content'] = Timber::get_posts($post->meta('season_content'));
+//$context['season_content'] = $season_content ? array_map(function($post_id) {
+//    return new Timber\Post($post_id);
+//}, wp_list_pluck($season_content, 'ID')) : [];
+$context['is_admin'] = current_user_can('administrator');
 
 require_once get_template_directory() . '/lib/wp-timber/timber--comment-form.php';
 
